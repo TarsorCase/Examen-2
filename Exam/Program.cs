b9c5b89e5788e7ad989e8bbb6aeff95505d88e8d
TarsorCase <gerardgonzalez41@gmail.com>
2024-04-23 15:47:33 -0400

    Subiendo Program.cs

---
+++ Exam/Program.cs
@@ -0,0 +1,94 @@
+﻿int yes = 1; 
+while(yes == 1){
+  Console.WriteLine("\n***** CALCULADORA *****");
+  Console.WriteLine("1 ---> MRU\n2 ---> MRUA\nSeleccione una opcion (1 - 2): ");
+  int optionMr = Convert.ToInt32(Console.ReadLine());
+  switch (optionMr)
+  {
+    case 1:  
+    Console.WriteLine("1 ---> Distancia\n2 ---> Velocidad\n3 ---> Tiempo\nSeleccione una opcion (1 - 2 - 3): ");
+    int optionMru = Convert.ToInt32(Console.ReadLine());
+    int distancia;
+    int tiempo;
+    int velocidad;
+    int pos_ini;
+    double pos_fin;
+    int velo_ini;
+    int velo_fin;
+    int aceleracion;
+      switch (optionMru){
+      case 1:
+      Console.WriteLine("Ingrese la velocidad: ");
+      velocidad = Convert.ToInt32(Console.ReadLine());
+      Console.WriteLine("Ingrese la tiempo: ");
+      tiempo = Convert.ToInt32(Console.ReadLine());
+      distancia = velocidad * tiempo;
+      Console.WriteLine($"La distancia es: {distancia}"); 
+      break;
+      case 2:
+      Console.WriteLine("Ingrese la distancia: ");
+      distancia = Convert.ToInt32(Console.ReadLine());
+      Console.WriteLine("Ingrese el tiempo: ");
+      tiempo = Convert.ToInt32(Console.ReadLine());
+      velocidad = distancia / tiempo;
+      Console.WriteLine($"La velocidad es: {velocidad}"); 
+      break;
+      case 3:
+      Console.WriteLine("Ingrese la distancia: ");
+      distancia = Convert.ToInt32(Console.ReadLine());
+      Console.WriteLine("Ingrese la velocidad: ");
+      velocidad = Convert.ToInt32(Console.ReadLine());
+      tiempo = distancia / velocidad;
+      Console.WriteLine($"La velocidad es: {tiempo}"); 
+      break;
+      }
+    break;
+    case 2:
+      Console.WriteLine("1 ---> Posicion final\n2 ---> Velocidad final\n3 ---> Tiempo\nSeleccione una opcion (1 - 2 - 3): ");
+      int optionMrua = Convert.ToInt32(Console.ReadLine());
+      switch (optionMrua){  
+      case 1:
+      Console.WriteLine("Ingrese la posicion inicial: ");
+      pos_ini = Convert.ToInt32(Console.ReadLine());
+      Console.WriteLine("Ingrese la velocidad inicial: ");
+      velo_ini = Convert.ToInt32(Console.ReadLine());
+      Console.WriteLine("Ingrese la tiempo: ");
+      tiempo = Convert.ToInt32(Console.ReadLine());
+      Console.WriteLine("Ingrese la aceleracion: ");
+      aceleracion = Convert.ToInt32(Console.ReadLine());
+      pos_fin = pos_ini+velo_ini*tiempo+0.5*aceleracion*Math.Pow(tiempo, 2);
+      Console.WriteLine($"La posicion final es: {pos_fin}"); 
+      break;
+      case 2:
+      Console.WriteLine("Ingrese la velocidad inicial: ");
+      velo_ini = Convert.ToInt32(Console.ReadLine());
+      Console.WriteLine("Ingrese la tiempo: ");
+      tiempo = Convert.ToInt32(Console.ReadLine());
+      Console.WriteLine("Ingrese la aceleracion: ");
+      aceleracion = Convert.ToInt32(Console.ReadLine());
+      velo_fin = velo_ini+ aceleracion* tiempo;
+      Console.WriteLine($"La velocidad es: {velo_fin}"); 
+      break;
+      case 3:
+      Console.WriteLine("Ingrese la velocidad final: ");
+      velo_fin = Convert.ToInt32(Console.ReadLine());
+      Console.WriteLine("Ingrese la velocidad inicial: ");
+      velo_ini = Convert.ToInt32(Console.ReadLine());
+      Console.WriteLine("Ingrese la aceleracion: ");
+      aceleracion = Convert.ToInt32(Console.ReadLine());
+      tiempo = (velo_fin - velo_ini) / aceleracion;
+      Console.WriteLine($"El timepo es: {tiempo}"); 
+      break;
+      }
+    break;
+  }
+  Console.WriteLine("Would you want to continue making calculations? 1 --> yes | 2 --> no\n");
+  yes = Convert.ToInt32(Console.ReadLine());
+}
+if (yes == 2)
+{
+  Console.WriteLine("\nThank you for using this calculator :)");
+  Console.WriteLine(" /\\_/\\");
+  Console.WriteLine("( o.o )");  
+  Console.WriteLine(" > ^ <");
+}
